c29edee944f8fa429a6836e69b27f7d6037431ce
hwan12321 <kdh643737@gmail.com>
2024-12-11 01:23:40 +0900

    Feat: Login 페이지/소셜 로그인 버튼 추가

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,21 +1,71 @@
-
+import classNames from "classnames";
+import style from "./Login.module.scss";
 
 const KAKAO_REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
-const KAKAO_REDIRECT_URI = `http://localhost:3000/auth`;
+const NAVER_REST_API_KEY = process.env.REACT_APP_NAVER_REST_API_KEY;
+const GOOGLE_REST_API_KEY = process.env.REACT_APP_GOOGLE_REST_API_KEY;
+const REDIRECT_URI = `http://localhost:3000/auth`;
 
 const Login = () => {
 
-    console.log(process.env.REACT_APP_KAKAO_REST_API_KEY);
-
     const onClickKakaoLogin = () => {
-        window.location.href= `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+        window.location.href= `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
     }
 
-    return (<div>
-        <button type="button" onClick={onClickKakaoLogin}>
-            카카오 로그인
-        </button>
-    </div>)
+    const onClickNaverLogin = () => {
+        window.location.href= `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_REST_API_KEY}&state=flase&redirect_uri=${REDIRECT_URI}`;
+    }
+
+    const onClickGoogleLogin = () => {
+        window.location.href= `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=email+profile`;
+    }
+
+    return (
+        <div id="wrap">
+            <div className="introTitle">
+                <img src="/images/mainImage.png" id="logoImg" />
+            </div>
+            <section>
+                <h1 className="main-title">
+                    WELCOME TO YOUR<br/>HEALTH PARTNER VITABUDDY
+                </h1>
+                <div className="loginForm">
+                    <form>
+                        <label htmlFor="id">아이디</label>
+                        <br />
+                        <input type="text" id="id" name="id" className="formTxt" placeholder="아이디" required />
+                        <br />
+                        <label htmlFor="pwd">비밀번호</label>
+                        <br />
+                        <input type="password" id="pwd" name="pwd" className="formTxt" placeholder="비밀번호" required />
+                        <br />
+                        <div className={style.login_btn_wrap}>
+                            <div className={style.social_login_btn_wrap}>
+                                <button type="button" className={style.naver_btn} onClick={onClickNaverLogin}/>  
+                                <button type="button" className={style.kakao_btn} onClick={onClickKakaoLogin}/>
+                                <button type="button" className={style.google_btn} onClick={onClickGoogleLogin}/>
+                            </div>
+                            <button type="submit" id="loginSubmit" className={classNames("btn","btnFilled",style.module_login_btn)}>로그인</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+            <br />
+            <section>
+                <div>
+                    <p>아직 회원이 아니신가요?</p>
+                    <a href="/member/register" className="btn btnFilled">
+                        회원가입
+                    </a>
+                </div>
+                <div>
+                    <a href="/">
+                        둘러보기
+                    </a>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Login;
